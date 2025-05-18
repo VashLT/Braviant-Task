219f4d215691ab01e1981e2451506c52da43d691
VashLT <mcvicksilvaa@gmail.com>
2025-05-18 17:41:23 -0500

    fix deploy

--- apps/host/webpack.prod.config.ts
+++ apps/host/webpack.prod.config.ts
@@ -10,7 +10,7 @@ export default withModuleFederation(
   {
     ...config,
     remotes: [
-      ['login', '/login/remoteEntry.js'],
+      ['login', '/login/remoteEntry.mjs'],
     ]
   },
   { dts: false }
